7bec9d828c97999013b72ad9b555ac6bbc12d1c0
cjwe <christajwest@gmail.com>
2022-05-27 14:12:28 +0200

    Removed package-lock, updated main page responsiveness, added correct Image tag to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,82 @@
+import Image from "next/image"
+import placeholder from "../public/placeholder.png"
+
 const Footer = () => {
   return (
-    <div>
-      <h2>Beela Footer</h2>
-      <p>Copyright 2022 Beela</p>
-    </div>
+    <>
+      <footer className="w-100 py-4 flex-shrink-0">
+        <div className="container py-4">
+          <div className="row gy-4 gx-5">
+            <div className="col-lg-6 col-md-6">
+              <div>
+                <h5 className="h1 e">Our Mission</h5>
+                <p className="small text-muted">
+                  We are committed to decreasing the unemployment gap between foreign-born women and
+                  native Swedes. We empower our community to define their path, get unblocked, and
+                  trailblaze into tech careers.{" "}
+                </p>
+                <p className="small text-muted mb-0">
+                  &copy; Copyrights. All rights reserved.{" "}
+                  <a className="text-primary" href="#">
+                    Bootstrapious.com
+                  </a>
+                </p>
+              </div>
+
+              <div>
+                <h5 className="h1 e">Our Partners</h5>
+                <Image src={placeholder} className="rounded-circle" alt="Cinque Terre" />
+                &nbsp;
+                <Image src={placeholder} className="rounded-circle" alt="Cinque Terre" />
+                &nbsp;
+                <Image src={placeholder} className="rounded-circle" alt="Cinque Terre" />
+                &nbsp;
+                <Image src={placeholder} className="rounded-circle" alt="Cinque Terre" />
+              </div>
+            </div>
+
+            <div className="col-lg-2 col-md-6">
+              <div>
+                <h5 className="mb-3">Sitemap</h5>
+                <ul className="list-unstyled ">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Get started</a>
+                  </li>
+                  <li>
+                    <a href="#">FAQ</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-lg-2 col-md-6">
+              <div>
+                <h5 className="mb-3">Contact Us</h5>
+                <ul className="list-unstyled ">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Get started</a>
+                  </li>
+                  <li>
+                    <a href="#">FAQ</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,13 +23,14 @@ const Home: NextPage = () => {
               <div className="col d-flex flex-column align-items-center border p-5">
                 <div className="p-3">
                   <h1>Beela best value proposition.</h1>
+                  <div className="p-3">
+                    <p id="header-text">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                      incididunt ut labore et dolore magna ali
+                    </p>
+                  </div>
                 </div>
-                <div className="p-3">
-                  <p id="header-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.
-                  </p>
-                </div>
+
                 <div className="p-3">
                   <button type="button" className="btn btn-dark">
                     Join Our Slack Community
@@ -55,33 +56,39 @@ const Home: NextPage = () => {
               </div>
 
               <div className="container">
-                <div className="card-deck d-flex flex-row">
-                  <div className="card m-3">
-                    <div className="card-body">
-                      <p className="card-title">
-                        <span>Gain Skills</span>
-                      </p>
-                      <p className="card-text">
-                        Explore various tech fields and learn relevant skills.
-                      </p>
+                <div className="row row-cols-1 row-cols-md-3">
+                  <div className="col w-75 mb-4">
+                    <div className="card h-100">
+                      <div className="card-body">
+                        <p className="card-title">
+                          <span>Gain Skills</span>
+                        </p>
+                        <p className="card-text">
+                          Explore various tech fields and learn relevant skills.
+                        </p>
+                      </div>
                     </div>
                   </div>
-                  <div className="card m-3">
-                    <div className="card-body">
-                      <p className="card-title">
-                        <span>Get Experience</span>
-                      </p>
-                      <p className="card-text">
-                        Put your skills to work with projects from our partners.
-                      </p>
+                  <div className="col mb-4">
+                    <div className="card h-100">
+                      <div className="card-body">
+                        <p className="card-title">
+                          <span>Get Experience</span>
+                        </p>
+                        <p className="card-text">
+                          Put your skills to work with projects from our partners.
+                        </p>
+                      </div>
                     </div>
                   </div>
-                  <div className="card m-3">
-                    <div className="card-body">
-                      <p className="card-title">
-                        <span>Land a Job</span>
-                      </p>
-                      <p className="card-text">Receive unparalleled mentorship and resources.</p>
+                  <div className="col mb-4">
+                    <div className="card h-100">
+                      <div className="card-body">
+                        <p className="card-title">
+                          <span>Land a Job</span>
+                        </p>
+                        <p className="card-text">Receive unparalleled mentorship and resources.</p>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -104,9 +111,9 @@ const Home: NextPage = () => {
                 </div>
               </div>
 
-              <div className="container">
+              <div className="container align-content-center">
                 <div
-                  className="d-flex flex-row align-items-center justify-content-center "
+                  className="d-flex flex-row flex-wrap align-items-center justify-content-center flex-md-nowrap p-3"
                   id="video"
                 >
                   <div className="m-3">
@@ -116,12 +123,12 @@ const Home: NextPage = () => {
                     <p>Our flagship programme offers 1:1 mentorship, along with resources.</p>
                   </div>
                 </div>
-                <hr />
               </div>
+              <hr />
 
               <div className="container">
-                <div className="row">
-                  <div className="col pl-5 pt-5 pr-5">
+                <div className="d-flex flex-row flex-wrap align-items-center justify-content-center flex-md-nowrap p-3">
+                  <div className="p-3">
                     <span>Become a Mentee</span>
                     <p>
                       Whether you’re exploring a tech field or applying to jobs, a mentor can help
@@ -131,7 +138,7 @@ const Home: NextPage = () => {
                       Learn more <i className="bi bi-arrow-right"></i>
                     </a>
                   </div>
-                  <div className="col pl-5 pt-5 pr-5">
+                  <div className="p-3">
                     <span>Become a Mentor</span>
                     <p>
                       Share your experiences and make a difference in a mentee’s career. Continue to
@@ -145,7 +152,6 @@ const Home: NextPage = () => {
               </div>
             </div>
           </div>
-
           {/* Testimonials */}
           <div className="row border text-center" id="testimonials">
             <div className="col align-items-center border p-5">
@@ -162,10 +168,10 @@ const Home: NextPage = () => {
               </div>
 
               <div className="container">
-                <div className="card-deck d-flex flex-row">
+                <div className="card-deck d-flex flex-row flex-wrap flex-md-nowrap justify-content-center">
                   <div className="card m-3">
                     <div className="card-body">
-                      <p className="card-title">
+                      <div className="card-title">
                         <div className="header">
                           <div className="avatar">
                             <Image src={avatar} alt="Avatar picture" id="avatar-image" />
@@ -178,12 +184,12 @@ const Home: NextPage = () => {
                           tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
                           veniam, quis nostrud exercitation.
                         </p>
-                      </p>
+                      </div>
                     </div>
                   </div>
                   <div className="card m-3">
                     <div className="card-body">
-                      <p className="card-title">
+                      <div className="card-title">
                         <div className="header">
                           <div className="avatar">
                             <Image src={avatar} alt="Avatar picture" id="avatar-image" />
@@ -196,12 +202,12 @@ const Home: NextPage = () => {
                           tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
                           veniam, quis nostrud exercitation.
                         </p>
-                      </p>
+                      </div>
                     </div>
                   </div>
                   <div className="card m-3">
                     <div className="card-body">
-                      <p className="card-title">
+                      <div className="card-title">
                         <div className="header">
                           <div className="avatar">
                             <Image src={avatar} alt="Avatar picture" id="avatar-image" />
@@ -209,7 +215,7 @@ const Home: NextPage = () => {
                         </div>
                         <span>Jane Doe</span>
                         <p className="text-muted"> UX Designer</p>
-                      </p>
+                      </div>
                       <p className="card-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -223,28 +229,24 @@ const Home: NextPage = () => {
           </div>
 
           {/* Podcast  */}
-          <div className="row border text-center" id="podcast">
+          <div className="row border align-items-center text-center" id="podcast">
             <div className="col align-items-center p-5">
-              <div className="d-flex flex-column ">
-                <div className="p-1">
-                  <h2>Beela’s Talk Podcast</h2>
-                </div>
+              <div className="p-1">
+                <h2>Beela’s Talk Podcast</h2>
               </div>
 
-              <div className="container">
-                <div
-                  className="d-flex flex-row align-items-center justify-content-center "
-                  id="video"
-                >
-                  <div className="m-3">
-                    <p>
-                      Our podcast is filled with inspiring stories from Swedish immigrants who have
-                      broken into tech. Catch up on all the buzz!
-                    </p>
-                  </div>
-                  <div className="m-3">
-                    <div className="rectangle"></div>
-                  </div>
+              <div
+                className="d-flex flex-row flex-wrap align-items-center justify-content-center flex-md-nowrap p-3"
+                id="video"
+              >
+                <div className="col m-3">
+                  <p>
+                    Our podcast is filled with inspiring stories from Swedish immigrants who have
+                    broken into tech. Catch up on all the buzz!
+                  </p>
+                </div>
+                <div className="col d-flex justify-content-center">
+                  <div className="rectangle"></div>
                 </div>
               </div>
 
