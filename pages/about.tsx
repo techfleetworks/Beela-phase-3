41d60f8295c7d1f4ef779720ff3b5da38da50518
mostafa-wahied <mostafawahied@gmail.com>
2022-06-01 16:43:22 -0700

    fix hero part in the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -17,12 +17,12 @@ const About: NextPage = () => {
           <div className="container">
             <div className="container" id="hero">
               <div className="container row">
-                <div className="col px-lg-5 m-5">
+                <div className="col p-lg-5 m-4">
 
                     <div className="col" id="about-beela">
                       <h1>About Beela</h1>
                     </div>
-                    <div className="col" id="about-paragraph">
+                    <div className="col-12" id="about-paragraph">
                       <p>
                         [OUR STORY / VISION / MISSION] Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                       </p>
@@ -33,8 +33,8 @@ const About: NextPage = () => {
                     <button type="button" className="btn btn-dark float-start mx-5">CTA</button>
                   </div>
                 </div>
-                <div className="col px-lg-5 m-5" id="about=picture">
-                  <div className="d-flex flex-row" id="picture">
+                <div className="col p-lg-5 m-4" id="about=picture">
+                  <div className="container flex-row" id="picture">
                     <div className="m-5">
                       <div className="rectangle"></div>
                     </div>
