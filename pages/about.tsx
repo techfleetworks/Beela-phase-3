a0b2c8f6270cbef960cc6a8fef09190d29e56a86
JoseSolorio <lodisolito@gmail.com>
2022-06-06 02:01:47 -0700

    another update of about us

--- pages/about.tsx
+++ pages/about.tsx
@@ -29,6 +29,79 @@ const About: NextPage = () => {
             </div>
          </div>
 
+         <div>
+         <div className="row border text-center" id="steps">
+            <div className="col align-items-center border p-5">
+            
+
+              <div className="container">
+                <div className="row row-cols-1 row-cols-md-3">
+                  <div className="col mb-4">
+                    <div className="card h-100">
+                      <div className="card-body">
+                        <p className="card-title">
+                          <span className="text-primary">Empower</span>
+                        </p>
+                        <p className="card-text">
+                        We help immigrant women and non-binary people to achieve all their potential through coaching, mentoring, and networking.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col mb-4">
+                    <div className="card h-100">
+                      <div className="card-body">
+                        <p className="card-title">
+                          <span  className="text-primary">Inform</span>
+                        </p>
+                        <p className="card-text">
+                        We spread the word about what’s happening in the tech world: bootcamps, code academies, courses, 
+and events.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col mb-4">
+                    <div className="card h-100">
+                   
+                      <div className="card-body">
+                        <p className="card-title">
+                          <span  className="text-primary">Inspire</span>
+                        </p>
+                        <p className="card-text">We share stories about people who have built a career in tech, as well as employers who have created opportunities 
+for newcomers.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+         </div>
+
+         <div  className={`container-fluid ${styles.abouthero}`}>
+             <div className="container">
+             <div className=" row"> 
+             
+                <div className="col-md-6">
+                <Image
+                          
+                            src="https://via.placeholder.com/469x303.png"
+                            alt="Picture of the author"
+                            width={469}
+                            height={303}
+                            />
+                </div>
+
+                <div className="col-md-6">
+                    <h2 className="text-primary">Our Community </h2>
+                    <p className="B1">To date, two cohorts have gone through our Pollination Mentoring Programme. Many of the people in the programme landed jobs! We are excited to build on this success and grow our community. </p>
+                </div>
+
+            </div>
+            </div>
+         </div>
+
    </div>
   )
 }
