306d6684a9d75aa40a617598146bb29b0200a87c
cjwe <christajwest@gmail.com>
2022-06-15 15:48:43 +0200

    Add stylesheet to document head

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="stylesheet" href="..." />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossOrigin="anonymous"
+        />
       </Head>
       <body>
         <Main />
