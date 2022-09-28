e93600554b3e88219671245a45c6eafdc5f65124
Anca Puscasu <ancapuscasu@ymail.com>
2022-09-28 14:01:40 -0400

    Imported fonts from design system
    no_name_37: regular and light
    Montserrat: 400, 500, 500 Italic, 600

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,12 @@ export default function Document() {
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossOrigin="anonymous"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;1,500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
