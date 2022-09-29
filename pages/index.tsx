df53f0b99267f207b6023eda6aded30606e53ae1
JoseSolorio <lodisolito@gmail.com>
2022-09-28 23:13:30 -0700

    Test1
    Test1

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,6 +40,7 @@ const Home: NextPage = () => {
         <Navbar />
 
         {/* Hero */}
+        
         <div className="container-fluid overflow-hidden" id="wrapper">
           <section id="section-hero">
             <Row className="text-center align-content-center justify-content-center" id="Hero">
