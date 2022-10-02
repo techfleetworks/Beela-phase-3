7a365f64e2e9dfac2fd594bce2978786eea8ed92
JoseSolorio <lodisolito@gmail.com>
2022-10-02 16:15:49 -0700

    organized SASS/CSS files structure
    organized sass files and css files into separate folders for organization purposes.
    
    PLEASE USE THE TESTING COMPONENT AS AN EXAMPLE OF HOW TO USE SASS IN your COMPONENT file.

--- components/sections/Testimonials.jsx
+++ components/sections/Testimonials.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from "react-responsive-carousel"
+
 export default class Testimonials extends Component {
   render() {
     return (
