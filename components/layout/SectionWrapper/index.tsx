a0b01a82e223c19a42cc32692f0cd7ee6bc07d20
Anca Puscasu <ancapuscasu@ymail.com>
2022-10-06 15:44:24 -0400

    Added max-width to SectionWrapper

--- components/layout/SectionWrapper/index.tsx
+++ components/layout/SectionWrapper/index.tsx
@@ -10,14 +10,19 @@ import styles from "./SectionWrapper.module.scss"
 type SectionWrapperProps = {
   color?: string
   children?: React.ReactNode
-  className?: string
   padding?: string
+  width?: number
 }
 
 export const SectionWrapper = ({
   color = "TRUE_WHITE",
   children,
+  width = 945,
   padding = "main",
 }: SectionWrapperProps) => {
-  return <section className={`${styles[color]} ${styles[padding]}`}>{children}</section>
+  return (
+    <section style={{ maxWidth: `${width}px` }} className={`${styles[color]} ${styles[padding]}`}>
+      {children}
+    </section>
+  )
 }
