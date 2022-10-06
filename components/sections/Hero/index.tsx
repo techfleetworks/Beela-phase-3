47b85ca8a5c1062733164c032863364e72e9d5d7
amanda_k <kim.amanda@outlook.com>
2022-10-06 11:31:32 -0700

    Updated Hero Section to Follow Figma Design

--- components/sections/Hero/index.tsx
+++ components/sections/Hero/index.tsx
@@ -17,29 +17,31 @@ export const Hero = () => {
             xl={5}
             className="p-5 hero-text-wrapper"
           >
-            <h2 className="text-color__berry">
-              Bee <span className="svg-underline"> supported</span>,
-              <span className="svg-underline"> empowered</span>, and
-              <span className="svg-underline"> connected</span>
-              {/* {data?.testimonials?.data[0].attributes?.title} */}
-            </h2>
-            <div className="left-center-text">
-              <p className="B1 p-3">
-                We offer free resources to help women and non-binary Swedish immigrants start tech
-                careers.
+            <div className="hero-left-section">
+              <h2 className="hero-heading">
+                Bee <span className="svg-underline"> supported</span>,
+                <span className="svg-underline"> empowered</span>, <br />
+                and <span className="svg-underline"> connected</span>
+                {/* {data?.testimonials?.data[0].attributes?.title} */}
+              </h2>
+
+              <p className="hero-text">
+                Our welcoming community helps women and non-binary Swedish immigrants break into tech.
               </p>
-              <Button
-                title="Join Our Slack Community"
-                href="https://join.slack.com/t/beela/shared_invite/zt-12kargaye-5R2bP0qwqNpFwPiiDAiwQQ"
-              />
+              <button className="hero-button">
+                <a href="https://join.slack.com/t/beela/shared_invite/zt-12kargaye-5R2bP0qwqNpFwPiiDAiwQQ">Join Our Slack Community</a>
+              </button>
             </div>
           </Col>
+
           <Col sm={{ order: 1 }} md={6} className="p-3">
+            <div className="hero-image">
             <Image
               src="/images/queenbees.png"
               alt="Illustraion of three women, one wearing glasses and a blue shirt, one wearing a shirt with the letter B and a crown, and the other in a polkadot shirt and a hijab."
               fluid
             />
+            </div>
           </Col>
         </Row>
       </Row>
