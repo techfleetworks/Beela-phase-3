6a02f99cc51e8c3c671fc3d53a6648b2636ca9af
incentFrank <frank.arinze@incentsoft.com>
2022-10-07 18:51:47 +0100

    Added stripe payment elements,created payment intents and methods

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // images: {
+  //   domains: ["via.placeholder.com"],
+  // },
   images: {
-    domains: ["via.placeholder.com"],
+    loader: 'akamai',
+    path: '',
   },
   reactStrictMode: true,
 }
