d55e6104616f99e7f17c43c6f3617b1e157fe2f0
Anca Puscasu <ancapuscasu@ymail.com>
2022-10-06 21:57:47 -0400

    Added button variants with styling

--- components/elements/Button/index.tsx
+++ components/elements/Button/index.tsx
@@ -1,26 +1,27 @@
+/* 
+Component used to wrap around <section></section> elements
+Accepted props:
+  color: "primary_lightBG", "primary_darkBG", "secondary_lightBG", "secondary_darkBG"
+*/
+
+import styles from "./Button.module.scss"
 
 interface ButtonProps {
-    style?: any
-    title: string
-    href?:string
-    children?: React.ReactNode
-  }
+  href?: string
+  title: string
+  type?: any
+  variant: string
+}
 
-export const Button = (props:ButtonProps) => {
-    const {title, href} = props
-    // Frank, Amanda and Anca was here
-    return (
-        <div className="">
-            <a
-                // href="https://join.slack.com/t/beela/shared_invite/zt-12kargaye-5R2bP0qwqNpFwPiiDAiwQQ"
-                href={href}
-                target="_blank"
-                rel="noreferrer"
-            >
-                <button type="button" className="btn button">
-                    {title}
-                </button>
-            </a>
-        </div>
-    )
+export const Button = (props: ButtonProps) => {
+  const { href, title, type = "button", variant } = props
+  return (
+    <div className="">
+      <a href={href} target="_blank" rel="noreferrer">
+        <button type={type} className={`button ${styles[variant]}`}>
+          {title}
+        </button>
+      </a>
+    </div>
+  )
 }
