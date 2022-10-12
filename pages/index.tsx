d5a43dc3689d9ba9096f6d6d43b1b42af4130bed
JoseSolorio <lodisolito@gmail.com>
2022-10-12 07:00:20 -0700

    removed testing-component + added ending semicolon
    removed the testingocomonent on the index file and also added semicolon to the end of the importing statements, just a personal preference though.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,30 @@
-import type { NextPage } from "next"
-import Popper from "@popperjs/core"
-import Link from "next/link"
-import Head from "next/head"
-import Navbar from "../components/Navbar"
-import Footer from "../components/layout/Footer"
-import Testimonials from "../components/sections/Testimonials"
-import { Hero } from "../components/sections/Hero"
-import { TeamCard } from "../components/elements/TeamCard"
+import type { NextPage } from "next";
+import Popper from "@popperjs/core";
+import Link from "next/link";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import Footer from "../components/layout/Footer";
+import Testimonials from "../components/sections/Testimonials";
+import { Hero } from "../components/sections/Hero";
+import { TeamCard } from "../components/elements/TeamCard";
 // import avatar from "../public/avatar.png"
-import { SectionWrapper } from "../components/layout/SectionWrapper"
+import { SectionWrapper } from "../components/layout/SectionWrapper";
 
 // React-Bootstrap icons
-import { Spotify } from "react-bootstrap-icons"
+import { Spotify } from "react-bootstrap-icons";
 
 // React-Bootstrap imports
-import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
-import Card from "react-bootstrap/Card"
-import Container from "react-bootstrap/Container"
-import Image from "react-bootstrap/Image"
-import { wrap } from "module"
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+//import { wrap } from "module"
 
 // mock data
-import teamData from "../mockdata/team.json"
+//import teamData from "../mockdata/team.json"
 
-import TestingComponent from "../components/elements/testingcomponent"
+// import TestingComponent from "../components/elements/testingcomponent"
 // import { useTestimonialsQuery } from "../graphql/generated"
 // import { usePostsQuery } from "../graphql/generated"
 // import { fieldNameFromStoreName } from "@apollo/client/cache"
@@ -46,8 +46,6 @@ const Home: NextPage = () => {
       <main>
         <Navbar />
 
-        {/* TESTING COMPONENT THAT USES SASS  */}
-        <TestingComponent/>
 
         <div className="container-fluid overflow-hidden" id="wrapper">
           {/* Hero */}
