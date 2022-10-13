8e8b76eeca65d6c752dc46938f4e5e4455223dbe
whitneywind <78887901+whitneywind@users.noreply.github.com>
2022-10-13 15:45:23 -0700

    hero in progress

--- components/sections/Hero/index.tsx
+++ components/sections/Hero/index.tsx
@@ -3,50 +3,46 @@ import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 import Image from "react-bootstrap/Image"
 import { Button } from "../../elements/Button"
+import { SectionWrapper } from "../../layout/SectionWrapper"
 
+import styles from './Hero.module.scss'
+// giving SectionWrapper a padding of 'hero' gives it a weird margin
 export const Hero = () => {
   return (
-    <section id="section-hero">
-      <Row className="text-center align-content-center justify-content-center" id="Hero">
-        <Row xs={1} s={2} md={2} className="m-4 align-items-center justify-content-center">
-          <Col
-            xs={{ order: 2 }}
-            sm={10}
-            md={{ span: 6, order: 1 }}
-            lg={6}
-            xl={5}
-            className="p-5 hero-text-wrapper"
-          >
-            <div className="hero-left-section">
-              <h2 className="hero-heading">
-                Bee <span className="svg-underline"> supported</span>,
-                <span className="svg-underline"> empowered</span>, <br />
-                and <span className="svg-underline"> connected</span>
-                {/* {data?.testimonials?.data[0].attributes?.title} */}
-              </h2>
+    <SectionWrapper padding='' maxWidth={1200}>
+      <section>
+        <Row className="text-center" id={styles.Hero}>
+          <Row xs={1} s={2} md={2} className="align-items-center justify-content-center">
+            <Col>
+              <div className={styles['hero-left-section']}>
+                <h2 className={styles['hero-heading']}>
+                  Bee <span className="svg-underline"> supported</span>,
+                  <span className="svg-underline"> empowered</span>, <br />
+                  and <span className="svg-underline"> connected</span>
+                  {/* {data?.testimonials?.data[0].attributes?.title} */}
+                </h2>
 
-              <p className="hero-text">
-                Our welcoming community helps women and non-binary Swedish immigrants break into tech.
-              </p>
+                <p className={styles['hero-text']}>
+                  Our welcoming community helps women and non-binary Swedish immigrants break into tech.
+                </p>
 
-              <button className="hero-button">
-                <a href="https://join.slack.com/t/beela/shared_invite/zt-12kargaye-5R2bP0qwqNpFwPiiDAiwQQ">Join Our Slack Community</a>
-              </button>
+                <Button href="https://join.slack.com/t/beela/shared_invite/zt-12kargaye-5R2bP0qwqNpFwPiiDAiwQQ" title="Join Our Slack Community" variant='primary_lightBG' />
+              </div>
+            </Col>
 
-            </div>
-          </Col>
-
-          <Col sm={{ order: 1 }} md={6} className="p-3">
-            <div className="hero-image">
-            <Image
-              src="/images/queenbees.png"
-              alt="Illustraion of three women, one wearing glasses and a blue shirt, one wearing a shirt with the letter B and a crown, and the other in a polkadot shirt and a hijab."
-              fluid
-            />
-            </div>
-          </Col>
+            <Col sm={{ order: 1 }}>
+              <div className={styles['hero-image']}>
+              <Image
+                src="/images/queenbees.png"
+                alt="Illustration of three women, one wearing glasses and a blue shirt, one wearing a shirt with the letter B and a crown, and the other in a polkadot shirt and a hijab."
+                fluid
+                
+              />
+              </div>
+            </Col>
+          </Row>
         </Row>
-      </Row>
-    </section>
+      </section>
+    </SectionWrapper>
   )
 }
