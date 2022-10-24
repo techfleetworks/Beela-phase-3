6d6bbddb519d99183e15e7415a61de69323d20f2
Anca Puscasu <ancapuscasu@ymail.com>
2022-10-24 12:07:12 -0400

    Created PageWrapper component and wrapped entire application in it
    - contains Navbar, Footer and PageWrapper component which gives top margin to page contents to allow for the navbar height

--- pages/pollinationMentors.jsx
+++ pages/pollinationMentors.jsx
@@ -1,16 +1,12 @@
-import React from 'react'
+import React from "react"
 import Hero from "../components/sections/pollinationMentors/Hero/hero"
-import Navbar from "../components/layout/Navbar/index"
 import WhatToExpectAsaMentor from "../components/sections/pollinationMentors/whattoexpectasamentor/expectmentor"
-
-
 
 export default function pollinationMentors() {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
-      <WhatToExpectAsaMentor/>
+      <Hero />
+      <WhatToExpectAsaMentor />
     </div>
   )
 }
