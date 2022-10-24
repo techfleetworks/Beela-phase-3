9fcc65376592c73ff8744eb8d0d0ce43717fa3cc
JoseSolorio <lodisolito@gmail.com>
2022-10-23 22:45:54 -0700

    pollination mentors hero section

---
+++ pages/pollinationMentors.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Hero from "../components/sections/pollinationMentors/Hero/hero"
+import Navbar from "../components/layout/Navbar/index"
+
+
+
+
+export default function pollinationMentors() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
