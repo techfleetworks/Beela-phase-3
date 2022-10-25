7ed59d518e001e176ee10118c8044d807c1957dd
Anca Puscasu <ancapuscasu@ymail.com>
2022-10-25 12:03:18 -0400

    Moved donate page hero to own folder
    - styled Hero section
    -added swish content
    -moved Choose amount options to PaymentMethods component

---
+++ components/sections/donate/Hero/index.tsx
@@ -0,0 +1,80 @@
+import React, { FC, useState } from "react"
+import Image from "next/image"
+
+// component
+import { SectionWrapper } from "../../../layout/SectionWrapper"
+import { Button } from "../../../elements/Button"
+import Spacer from "../../../elements/Spacer"
+
+// scss
+import styles from "./Hero.module.scss"
+import ReactIf from "../../../ReactIf"
+import useToggle from "../../../../hooks/useToggle"
+import PaymentForm from "../../../PaymentMethods"
+
+export const Hero: FC<any> = ({}) => {
+  const [visible, initPayment, cancelPayment] = useToggle(false)
+  const [viewSwish, setViewSwish] = useState(false)
+
+  const handleContinueBtn = () => {
+    initPayment()
+  }
+  // Retrieving product price from stripe (inprogress)
+  return (
+    <>
+      <ReactIf condition={!visible}>
+        <SectionWrapper
+          padding="paymentform"
+          maxWidth={728}
+          className="mx-auto"
+          color="SECONDARY_CREAM"
+        >
+          <div className={styles.sectionContainer}>
+            <div className={styles.headingContainer}>
+              <h1 className="primary-berry">
+                Bee a<span className="svg-underline"> Supporter</span>
+              </h1>
+              <h2 className="primary-berry">
+                Your gift will help us guide more immigrant women and nonbinary people into tech
+              </h2>
+            </div>
+            <div className={styles.buttonContainer}>
+              <Button
+                className={styles.paymentButton}
+                variant="primary_lightBG"
+                title="Swish a Gift"
+                onClick={() => {
+                  setViewSwish(!viewSwish)
+                }}
+              />
+              <Button
+                className={styles.paymentButton}
+                variant="primary_darkBG"
+                title="Pay with Credit"
+                onClick={() => handleContinueBtn()}
+              />
+            </div>
+            {viewSwish && (
+              <div className={styles.swishContainer}>
+                <h5>Scan with your Swish app</h5>
+                <p className="B1">Swish number: 1234567890</p>
+                <Image
+                  src="../icons/qr-code.svg"
+                  alt="Swish QR code for making a donation to Beela"
+                  height={206}
+                  width={206}
+                  layout="intrinsic"
+                />
+              </div>
+            )}
+          </div>
+        </SectionWrapper>
+      </ReactIf>
+      <ReactIf condition={visible}>
+        <div>
+          <PaymentForm />
+        </div>
+      </ReactIf>
+    </>
+  )
+}
