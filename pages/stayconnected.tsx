87e17cf97e1e81275568b53b5dd3b16ea8785e0f
JoseSolorio <lodisolito@gmail.com>
2022-10-25 14:51:49 -0700

    merging-stay-connected-files
    fixing a stay-connected-issue

--- pages/stayconnected.tsx
+++ pages/stayconnected.tsx
@@ -2,7 +2,7 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
-import { PageWrapper } from "../components/layout/PageWrapper"
+import  PageWrapper  from "../components/layout/PageWrapper/index"
 
 //Components
 import Navbar from "../components/layout/Navbar/index"
