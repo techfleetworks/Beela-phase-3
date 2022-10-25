be923c808467071337e48592ec92ad4dcdf935cc
JoseSolorio <lodisolito@gmail.com>
2022-10-25 15:19:08 -0700

    renamed hero file and hero scss file

--- components/sections/stayconnected/hero/hero.jsx
+++ components/sections/stayconnected/hero/hero.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 
 import { SectionWrapper } from "../../../layout/SectionWrapper"
-import styles from "./Hero.module.scss"
+import styles from "./heero.module.scss"
 
 export default function hero() {
   return (
