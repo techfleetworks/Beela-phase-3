8f91ea43d56c850af386236709bdc66e552f1e20
Anca Puscasu <ancapuscasu@ymail.com>
2022-10-26 18:22:33 -0400

    fixed gh conflict

--- pages/pollinationMentors.jsx
+++ pages/pollinationMentors.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Hero from "../components/sections/pollination-mentor/Hero/hero"
+import Hero from "../components/sections/pollination-mentor/Hero"
 import WhatToExpectAsaMentor from "../components/sections/pollination-mentor/whattoexpectasamentor/expectmentor"
 
 export default function pollinationMentors() {
