fa2f02195a05796883410f647f78f607b1c50a3d
Anca Puscasu <ancapuscasu@ymail.com>
2022-10-25 22:26:13 -0400

    Created ApprenticeshipProgramme component
    Content not filled in yet

---
+++ components/sections/programmes/ApprenticeshipProgramme/index.jsx
@@ -0,0 +1,10 @@
+import styles from "./ApprenticeshipProgramme.module.scss"
+import { SectionWrapper } from "../../../layout/SectionWrapper"
+
+export default function ApprenticeshipProgramme() {
+  return (
+    <SectionWrapper padding="main" color="PRIMARY_CORAL" maxWidth={978}>
+      <h1>Hello</h1>
+    </SectionWrapper>
+  )
+}
