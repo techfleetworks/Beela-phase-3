76bd589623cc20c0433d1d2572264e9eae0878af
JoseSolorio <lodisolito@gmail.com>
2022-10-27 09:43:12 -0700

    forgot one aposterphy
    &apos; oopsis haha

--- components/sections/gdpr/index.tsx
+++ components/sections/gdpr/index.tsx
@@ -191,7 +191,7 @@ export const PriNotice = () => {
                 <p className="B1" style={{marginBottom: "1rem" , lineHeight: "170%"}}>We will only keep your personal information for as long as it is 
                 necessary for the purposes set out in this privacy notice, unless a longer retention period is required or permitted by law (such as tax, 
                 accounting, or other legal requirements). No purpose in this notice will require us keeping your personal information for longer than three 
-                (3) months past the termination of the user's account.</p>         
+                (3) months past the termination of the user&apos;s account.</p>         
                 <p className="B1" style={{marginBottom: "1rem" , lineHeight: "170%"}}>When we have no ongoing legitimate business need to process your personal
                 information, we will either delete or anonymise such information, or, if this is not possible (for example, because your personal information 
                 has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible.</p> 
