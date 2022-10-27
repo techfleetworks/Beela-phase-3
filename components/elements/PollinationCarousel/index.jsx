fcc99ec1c13f1485226971399c32c2cae1540450
Anca Puscasu <ancapuscasu@ymail.com>
2022-10-27 14:25:50 -0400

    Created Carousel Component

---
+++ components/elements/PollinationCarousel/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./PollinationCarousel.module.scss"
+
+import SectionWrapper from "../../layout/SectionWrapper"
+
+function PollinationCarousel(color) {
+  return <SectionWrapper color={color}></SectionWrapper>
+}
+
+export default PollinationCarousel
