08169a094b0b3ac9807d0b0c63da9b767facc736
Anca Puscasu <ancapuscasu@ymail.com>
2022-10-27 12:05:40 -0400

    removed React Fragment

--- components/sections/pollination-mentor/Hero/index.jsx
+++ components/sections/pollination-mentor/Hero/index.jsx
@@ -11,53 +11,51 @@ export default function Hero() {
   let showButton = data.session === true ? "" : "none"
 
   return (
-    <>
-      <SectionWrapper color="SECONDARY_CREAM" padding="pollination">
-        <div className={styles.sectionContainer}>
-          <div className={styles.headingContainer}>
-            <h1 className={`primary-berry h2 mb-3`}>Pollination For Mentors</h1>
-            <h3 className={`primary-berry ${styles.subheading}`}>{data.message}</h3>
-          </div>
-          <Container fluid className="p-0">
-            <Row className={styles.contentContainer}>
-              <Col className={styles.imgContainer}>
-                <Image
-                  height={405}
-                  width={427}
-                  layout="intrinsic"
-                  className={styles.img}
-                  src="images/pollination-mentors_hero.svg"
-                  alt="placeholder"
-                />
-              </Col>
-              <Col className={styles.textContainer}>
-                <h4 className={`mb-4 primary-berry ${styles.cta}`}>
-                  Ready to help others get into tech?{" "}
-                </h4>
-                <p className={`B1 ${styles.infoText}`}>
-                  Our Pollination Mentoring Programme provides a space to help empower mentees to
-                  blossom.
-                </p>
-
-                <ul className={`B1 mb-5 ${styles.list}`}>
-                  <li>4 month programme</li>
-                  <li>1-on-1 session with mentee twice a month</li>
-                  <li>Special Slack groups for mentors</li>
-                  <li>Networking opportunities</li>
-                  <li>Opportunity to refine your leadership and communication skills</li>
-                </ul>
-                <Button
-                  style={{ display: showButton }}
-                  className={styles.applyBtn}
-                  title="Start Your Application"
-                  href={buttonHref}
-                  variant="primary_lightBG"
-                />
-              </Col>
-            </Row>
-          </Container>
+    <SectionWrapper color="SECONDARY_CREAM" padding="pollination">
+      <div className={styles.sectionContainer}>
+        <div className={styles.headingContainer}>
+          <h1 className={`primary-berry h2 mb-3`}>Pollination For Mentors</h1>
+          <h3 className={`primary-berry ${styles.subheading}`}>{data.message}</h3>
         </div>
-      </SectionWrapper>
-    </>
+        <Container fluid className="p-0">
+          <Row className={styles.contentContainer}>
+            <Col className={styles.imgContainer}>
+              <Image
+                height={405}
+                width={427}
+                layout="intrinsic"
+                className={styles.img}
+                src="images/pollination-mentors_hero.svg"
+                alt="placeholder"
+              />
+            </Col>
+            <Col className={styles.textContainer}>
+              <h4 className={`mb-4 primary-berry ${styles.cta}`}>
+                Ready to help others get into tech?{" "}
+              </h4>
+              <p className={`B1 ${styles.infoText}`}>
+                Our Pollination Mentoring Programme provides a space to help empower mentees to
+                blossom.
+              </p>
+
+              <ul className={`B1 mb-5 ${styles.list}`}>
+                <li>4 month programme</li>
+                <li>1-on-1 session with mentee twice a month</li>
+                <li>Special Slack groups for mentors</li>
+                <li>Networking opportunities</li>
+                <li>Opportunity to refine your leadership and communication skills</li>
+              </ul>
+              <Button
+                style={{ display: showButton }}
+                className={styles.applyBtn}
+                title="Start Your Application"
+                href={buttonHref}
+                variant="primary_lightBG"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </SectionWrapper>
   )
 }
--- components/sections/programmes/Hero/index.jsx
+++ components/sections/programmes/Hero/index.jsx
@@ -6,48 +6,45 @@ import styles from "./Hero.module.scss"
 
 export default function Hero() {
   return (
-    <>
-      <SectionWrapper color="SECONDARY_CREAM" padding="pollination">
-        <div className={styles.sectionContainer}>
-          <div className={styles.headingContainer}>
-            <h1 className={`primary-berry mb-3`}>Beela Programmes</h1>
-          </div>
-          <Container fluid className="p-0">
-            <Row className={styles.contentContainer}>
-              <Col className={styles.textContainer}>
-                <h4 className={`primary-berry ${styles.cta}`}>How can Beela help?</h4>
-                <p className={`B1 ${styles.infoText}`}>
-                  We offer different programmes to fit your needs depending on where you are in your
-                  journey.
-                </p>
-
-                <ul className={`B1 ${styles.list}`}>
-                  <li>
-                    Our Pollination Mentoring Programme connects you with mentors for 1-on-1
-                    guidance
-                  </li>
-                  <li>
-                    Find projects and apprenticeships to hone your skills in the #announcements,
-                    #job-leads, and #projects-pro-bono Slack channels
-                  </li>
-                </ul>
-              </Col>
-              <Col className={styles.imgContainer}>
-                <Image
-                  height={368}
-                  width={459}
-                  layout="intrinsic"
-                  className={styles.img}
-                  src="images/programmes_hero.svg"
-                  alt="placeholder"
-                />
-              </Col>
-            </Row>
-          </Container>
+    <SectionWrapper color="SECONDARY_CREAM" padding="pollination">
+      <div className={styles.sectionContainer}>
+        <div className={styles.headingContainer}>
+          <h1 className={`primary-berry mb-3`}>Beela Programmes</h1>
         </div>
-        <div className={styles.sectionDivider}></div>
-        <PollinationMentoringProgramme />
-      </SectionWrapper>
-    </>
+        <Container fluid className="p-0">
+          <Row className={styles.contentContainer}>
+            <Col className={styles.textContainer}>
+              <h4 className={`primary-berry ${styles.cta}`}>How can Beela help?</h4>
+              <p className={`B1 ${styles.infoText}`}>
+                We offer different programmes to fit your needs depending on where you are in your
+                journey.
+              </p>
+
+              <ul className={`B1 ${styles.list}`}>
+                <li>
+                  Our Pollination Mentoring Programme connects you with mentors for 1-on-1 guidance
+                </li>
+                <li>
+                  Find projects and apprenticeships to hone your skills in the #announcements,
+                  #job-leads, and #projects-pro-bono Slack channels
+                </li>
+              </ul>
+            </Col>
+            <Col className={styles.imgContainer}>
+              <Image
+                height={368}
+                width={459}
+                layout="intrinsic"
+                className={styles.img}
+                src="images/programmes_hero.svg"
+                alt="placeholder"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className={styles.sectionDivider}></div>
+      <PollinationMentoringProgramme />
+    </SectionWrapper>
   )
 }
