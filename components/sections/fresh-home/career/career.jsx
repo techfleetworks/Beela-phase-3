bcf0751182dceeb223f06b6cc76e1ae6c29e09cb
amanda_k <kim.amanda@outlook.com>
2022-10-31 19:41:24 -0700

    Homepage: 3 Steps Career Styling Section

--- components/sections/fresh-home/career/career.jsx
+++ components/sections/fresh-home/career/career.jsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap"
+import {  Row, Col } from "react-bootstrap"
 import { SectionWrapper } from "../../../layout/SectionWrapper"
 import styles from "./career.module.scss"
 
@@ -6,15 +6,13 @@ import BeeCard from "../../../elements/BeeCard"
 
 export default function Career() {
   return (
-    <SectionWrapper padding="main" maxWidth={880}>
-      <Container className="p-0">
+    <SectionWrapper padding="main" maxWidth={875}>
         <h2 className="primary-berry text-center my-2">3 Steps to Start a New Career</h2>
 
-        <p className={`B2 ${styles.text}`}>
+        <p className={`B1 ${styles.text}`}>
           We know that it’s not easy to change careers and move to a new country.
           <br />
-          We are here to support you every step of the way.
-          <span className={styles.italics}> Bzz: tech isn’t just coding!</span>
+          We are here to support you every step of the way. Bzz: tech isn’t just coding!
         </p>
         <Row className={styles.cards}>
           <Col sm={12} md={4} className="d-flex justify-content-center">
@@ -39,7 +37,6 @@ export default function Career() {
             />
           </Col>
         </Row>
-      </Container>
     </SectionWrapper>
   )
 }
