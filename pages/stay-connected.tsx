27947701436ac117b8b65a8f7d5d77c6a760a123
Anca Puscasu <ancapuscasu@ymail.com>
2022-10-31 21:02:11 -0400

    Added CommunityLayout sections to stay-connected page

--- pages/stay-connected.tsx
+++ pages/stay-connected.tsx
@@ -6,7 +6,6 @@ import PageWrapper from "../components/layout/PageWrapper/index"
 //Components
 
 import Hero from "../components/sections/stay-connected/Hero"
-import JoinOurCommunity from "../components/sections/stay-connected/JoinOurCommunity"
 import CommunityLayout from "../components/layout/CommunityLayout"
 import SupportBeela from "../components/sections/about/SupportBeela"
 
@@ -20,9 +19,27 @@ const StayConnected: NextPage = () => {
           content="Our welcoming community helps women and non-binary Swedish immigrants break into tech."
         />
       </Head>
-
       <Hero />
-      <JoinOurCommunity />
+      <CommunityLayout
+        altText="Inspire, Empower, Repeat written in a fun font"
+        blueTitle
+        color="SECONDARY_CORNFLOWER"
+        imgSource="../images/inspire.jpg"
+        newsletter
+        reverse
+        sectionTitle="Newsletter"
+        text="Let’s stay connected. Subscribe to our newsletter to stay up-to-date on resources and events."
+      />
+
+      <CommunityLayout
+        altText="Beela's Talk Podcast logo"
+        blueTitle
+        color="PRIMARY_CORAL"
+        imgSource="../images/beelas-talk.png"
+        icons
+        sectionTitle="Podcast"
+        text="Our podcast is filled with inspiring stories from Swedish immigrants who have broken into tech. Catch up on all the buzz!"
+      />
       <SupportBeela />
     </div>
   )
