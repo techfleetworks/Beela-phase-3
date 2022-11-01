6c966137dbf632a0a224ef8d0a91f7379368f3c4
rusevaIvelina <iva.ruseva14@gmail.com>
2022-11-01 17:46:35 -0400

    test

--- components/FAQ/index.jsx
+++ components/FAQ/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Container from "react-bootstrap/Container";
 import Accordion from "react-bootstrap/Accordion";
 import { SectionWrapper } from "../layout/SectionWrapper";
+import styles from "./faq.module.scss"
 
 export default function FAQ ({data, title}) {
  
@@ -11,13 +12,12 @@ export default function FAQ ({data, title}) {
       <SectionWrapper color="SECONDARY_CREAM" >
       <Container fluid className="py-5">
        <Container>
-        <h3 className="no_name_37 text-center">{title}</h3>
+        <h3 className={styles.title}>{title}</h3>
         <Accordion defaultActiveKey="0" className="my-5">
         {data.map((faq, id) => (
           <Accordion.Item eventKey={id} key={id}>
-            <Accordion.Header className="mb-0 no_name_37">
-            <span className='primary-berry' 
-               style={{fontWeight: "600", fontSize: "18px", fontFamily: "Montserrat"}}>
+            <Accordion.Header >
+            <span className={styles.questions} >
                {faq.question}</span>
             </Accordion.Header>
 
