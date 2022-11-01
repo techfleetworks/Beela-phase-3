146ffad92fea12a7d756070ef46863a3ba6cb0d3
Anca Puscasu <ancapuscasu@ymail.com>
2022-10-31 21:01:32 -0400

    styled stay-connected page and cleaned up folder structure

--- components/sections/stay-connected/Hero/index.jsx
+++ components/sections/stay-connected/Hero/index.jsx
@@ -1,37 +1,36 @@
 import Image from "next/image"
-
+import { Container, Row, Col } from "react-bootstrap"
 import { SectionWrapper } from "../../../layout/SectionWrapper"
 import styles from "./Hero.module.scss"
 
+import JoinOurCommunity from "../JoinOurCommunity"
+
 export default function Hero() {
   return (
-    <SectionWrapper color="SECONDARY_CREAM" padding="none">
+    <SectionWrapper color="SECONDARY_CREAM" padding="pollination">
       <div className={styles.sectionContainer}>
-        <h2 className="primary-berry" style={{ marginTop: "2rem", marginBottom: "2rem" }}>
-          Stay Connected
-        </h2>
+        <h2 className={`primary-berry ${styles.heading}`}>Stay Connected</h2>
         <div className={styles.imgContainer}>
           <Image
             height={369}
             width={428}
             layout="intrinsic"
             className={styles.img}
-            src="images/stayconnected-hero.svg"
-            alt="placeholder"
+            src="images/stay-connected_hero.png"
+            alt="Two women working together at a desk"
           />
         </div>
         <div className={styles.textContainer}>
-          <h4 style={{ marginTop: "2rem", marginBottom: "2rem" }} className={styles.h4}>
-            Come Join Us!
-          </h4>
-          <p className="B1 mb-4" style={{ lineHeight: "170%" }}>
-            We`&apos;`re ready to support your tech journey in Sweden in a variety of ways. We want
-            to welcome you and encourage you along the way.
+          <h4 className={`primary-berry ${styles.subheading}`}>Come join us!</h4>
+          <p className="B1">
+            We&apos;re ready to support your tech journey in Sweden in a variety of ways. We want to
+            welcome you and encourage you along the way.
           </p>
-          <p className="B1 mt-4 pt-4">We can`&apos;`t wait to connect with you.</p>
+          <p className="B1">We can&apos;t wait to connect with you.</p>
         </div>
       </div>
       <div className={styles.sectionDivider}></div>
+      <JoinOurCommunity />
     </SectionWrapper>
   )
 }
--- components/sections/stay-connected/JoinOurCommunity/index.jsx
+++ components/sections/stay-connected/JoinOurCommunity/index.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { Container, Row, Col } from "react-bootstrap"
 
 import { Button } from "../../../elements/Button/index"
 import { SectionWrapper } from "../../../layout/SectionWrapper"
@@ -8,48 +9,56 @@ import styles from "./JoinOurCommunity.module.scss"
 export default function JoinOurCommunity() {
   return (
     <SectionWrapper color="SECONDARY_CREAM" padding="none">
-      <div className={styles.sectionContainer}>
-        <Image
-          height={534}
-          width={436}
-          className={styles.img}
-          src="images/LeftColumn.svg"
-          alt="placeholder"
-        />
+      <Container fluid className="p-o">
+        <Row className={styles.sectionContainer}>
+          <Col>
+            <Image
+              height={534}
+              width={436}
+              layout="intrinsic"
+              src="images/zen-computer-lady.png"
+              alt="A girl sitting cross-legged in a meditative pose with a laptop on her lap, a coffee mug beside her and calendar in the background. An icon of a watering can labeled 'Take Care' is pouring water on her head which is shown growing plants."
+            />
+          </Col>
 
-        <div className={styles.textContainer}>
-          <h2 className={styles.h2}>Join Our Community</h2>
-          <h4 style={{ marginTop: "4rem", marginBottom: "1rem" }} className={styles.h4}>
-            Our Community Uses Slack to Connect
-          </h4>
-          <p className="B1" style={{ lineHeight: "170%" }}>
-            If you’ve never used Slack before, it’s okay! It’s a discussion platform that many tech
-            companies use to communicate. We promise you’ll get the hang of it quickly. Here are
-            some of our most popular channels:
-          </p>
-          <ul style={{ marginBottom: "1rem" }}>
-            <li className={styles.list}></li>
-            <p className="B1" style={{ lineHeight: "170%" }}>
-              {" "}
-              · <b>#welcome</b> Introduce yourself and connect with other immigrants to Sweden.
-            </p>
-            <li className={styles.list}></li>
-            <p className="B1" style={{ lineHeight: "170%" }}>
-              {" "}
-              · <b>#encouragement</b> Ask questions, share your experiences, and receive support.{" "}
-            </p>
-            <li className={styles.list}></li>
-            <p className="B1" style={{ lineHeight: "170%" }}>
-              {" "}
-              · <b>#job-leads</b> Share and view job descriptions from employers that welcome
-              diverse applicants.{" "}
-            </p>
-          </ul>
-          <div className={styles.button}>
-            <Button href="/about" variant="primary_lightBG" title="Join Us On Slack" />
-          </div>
-        </div>
-      </div>
+          <Col className={styles.contentContainer}>
+            <h2 className={`primary-berry ${styles.heading}`}>Join Our Community</h2>
+            <div className={styles.textContainer}>
+              <h4 className={`primary-berry ${styles.subheading}`}>
+                Our community uses Slack to connect
+              </h4>
+              <div className={styles.infoContainer}>
+                <p className="B1">
+                  If you’ve never used Slack before, it’s okay! It’s a discussion platform that many
+                  tech companies use to communicate. We promise you’ll get the hang of it quickly.
+                  Here are some of our most popular channels:
+                </p>
+                <ul className={`B1 ${styles.list}`}>
+                  <li>
+                    <b>#welcome</b> Introduce yourself and connect with other immigrants to Sweden.
+                  </li>
+
+                  <li>
+                    <b>#encouragement</b> Ask questions, share your experiences, and receive
+                    support.
+                  </li>
+
+                  <li>
+                    <b>#job-leads</b> Share and view job descriptions from employers that welcome
+                    diverse applicants.
+                  </li>
+                </ul>
+              </div>
+              <Button
+                href="https://beela.slack.com/join/shared_invite/zt-12kargaye-5R2bP0qwqNpFwPiiDAiwQQ#/shared-invite/email"
+                variant="primary_lightBG"
+                title="Join Us On Slack"
+                className="mx-auto"
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </SectionWrapper>
   )
 }
